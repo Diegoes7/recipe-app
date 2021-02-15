351c582d6e5932be7e60b8f1748beacadd1e48b5
Diegoes7 <dimitar.i.dimitrov77@gmail.com>
2021-02-15 18:27:33 +0200

    Add key

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'ff590d89-48b9-4622-b7ae-b2da1f6b025b';
 export const MODAL_CLOSE_SEC = 2.5;
